f0475745978f56451b359cb73d5b0bfa3d1647f1
Cory Ferrier <ferriercory@gmail.com>
2016-11-12 14:31:47 -0500

    Added js files for each of the db-objects, removed default stuff and added login button

--- server/main.js
+++ server/main.js
@@ -1,4 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/listings.js';
+import '../imports/api/messageChains.js';
+import '../imports/api/listings.js';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
   // code to run on server at startup
