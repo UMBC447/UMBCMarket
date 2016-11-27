63c890c68d9acd7417a7511ed6b53861a368436d
Cory Ferrier <ferriercory@gmail.com>
2016-11-27 17:00:11 -0500

    Major improvements to Header and general UI

--- imports/ui/header.js
+++ imports/ui/header.js
@@ -6,7 +6,34 @@ import { Messages } from '../api/messages.js';
 import './ApplicationLayout.html';
 
 Template.header.helpers({
-
+    getRouteName() {
+        var routeName = Router.current().route.getName();
+        switch (routeName){
+            case 'messages':
+                return 'Messages';
+                break;
+            case 'search_results/:_search_key':
+                return 'Search Results';
+                break;
+            case 'profile/:_id':
+                return 'Profile';
+                break;
+            case 'conversation/:_id':
+                return 'Conversation';
+                break;
+            case 'home':
+                return 'Home';
+                break;
+            case 'listing/:_id':
+                return 'Listing';
+                break;
+            case 'new_message/:_id':
+                return 'Send Message';
+                break;
+        }
+        console.log(routeName);
+        return routeName;
+    }
 });
 
 Template.header.events({
@@ -15,9 +42,12 @@ Template.header.events({
         Router.go('home');
     },
     'click .viewMessages': function () {
+        event.preventDefault();
+
         Router.go('messages');
     },
-    'click .viewProfile': function () {
+    'click .viewProfile': function (event) {
+        event.preventDefault();
         Router.go('profile/:_id', {_id: Meteor.userId});
     },
     'click #login-buttons-logout': function (event) {
